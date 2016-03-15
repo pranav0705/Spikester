0eecf780c66b922e711de322dad289fe80ffa274
“Tanmay <tkale1@binghamton.edu>
2016-03-15 05:57:12 -0400

    added sound buttons and background to the Start screen

--- p06-Spikester/ViewController.m
+++ p06-Spikester/ViewController.m
@@ -13,10 +13,34 @@
 @end
 
 @implementation ViewController
-
+@synthesize  sound,credit,shop;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIGraphicsBeginImageContext(self.view.frame.size);
+    [[UIImage imageNamed:@"2.jpg"] drawInRect:self.view.bounds];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+    
+    
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    CGFloat screenWidth = screenSize.width;
+    CGFloat screenHeight = screenSize.height;
+    
+    int startX=screenWidth/6;
+    
+    sound = [[UIView alloc] initWithFrame:CGRectMake((startX/2)+1, screenHeight-110, 70, 70)];
+    [self.view addSubview:sound];
+    [sound setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"abc.png"]]];
+
+    shop = [[UIView alloc] initWithFrame:CGRectMake((startX*3)+1-(startX/2), screenHeight-110, 70, 70)];
+    [self.view addSubview:shop];
+    [shop setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"abc.png"]]];
+    
+    credit = [[UIView alloc] initWithFrame:CGRectMake((startX*5)+1-(startX/2), screenHeight-110, 70, 70)];
+    [self.view addSubview:credit];
+    [credit setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"abc.png"]]];
 }
 
 - (void)didReceiveMemoryWarning {
